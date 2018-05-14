7b375e830b97f2922c7b888f2dd4fa49502739d7
Daniele Cortesi <dani.c93@hotmail.it>
2018-05-14 12:16:26 +0200

    add scripts and requirements

---
+++ train_acer.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+PYTHONPATH=. python baselines/acer/run_rogue.py -f default_rogue_flags.cfg $*
---
+++ video_acer.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+PYTHONPATH=. python envs/rogue/video_acer.py -f default_rogue_flags.cfg $*
