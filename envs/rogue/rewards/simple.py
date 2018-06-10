cc290cfc28df25c0c8df35fe080e254b5d6f7316
Daniele Cortesi <dani.c93@hotmail.it>
2018-06-10 10:19:03 +0200

    transfer reward generators to roguelib

--- envs/rogue/rewards/simple.py
+++ envs/rogue/rewards/simple.py
@@ -3,39 +3,15 @@ from roguelib_module.rewards import RewardGenerator
 from . import register
 
 
-class StairsOnly_RewardGenerator(RewardGenerator):
+"""
+Define in this file custom reward generators in the following way:
 
-    def get_value(self, frame_history):
-        old_info = frame_history[-2]
-        new_info = frame_history[-1]
-        if new_info.statusbar["dungeon_level"] > old_info.statusbar["dungeon_level"]:
-            self.goal_achieved = True
-            return 10
-        return 0
+class GeneratorName(Superclass):  # a common superclass is roguelib_module.rewards.RewardGenerator
+    # implementation
+    ...
 
+    
+register(GeneratorName)  # call this function to be able to use the generator by name string
 
-register(StairsOnly_RewardGenerator)
+"""
 
-
-class StairsOnly_NthLevel_RewardGenerator(RewardGenerator):
-    """Generates a reward of 10 whenever a higher level is reached.
-    If 'objective_level' is reached, declares the game won.
-    """
-
-    objective_level = 10
-
-    def reset(self):
-        super().reset()
-        self.last_level = 1
-
-    def get_value(self, frame_history):
-        last_frame = frame_history[-1]
-        if last_frame.statusbar["dungeon_level"] > self.last_level:
-            self.last_level = last_frame.statusbar["dungeon_level"]
-            if self.last_level >= self.objective_level:
-                self.goal_achieved = True
-            return 10
-        return 0
-
-
-register(StairsOnly_NthLevel_RewardGenerator)
